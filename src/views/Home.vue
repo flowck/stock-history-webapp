c7d686831f0b6158c5d85f84d9180deb1d368075
Firmino Changani <flowck96@gmail.com>
2019-04-13 10:55:52 +0100

    Build a prototype time series chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <vue-c3 :handler="handler" />
+  </div>
 </template>
 
 <script>
+import moment from "moment";
+import Vue from "vue";
+import VueC3 from "vue-c3";
+import { format } from "d3";
 export default {
   name: "Home",
   data() {
     return {
+      handler: new Vue(),
       dataset: [],
       isRequesting: false,
       hadError: ""
     };
+  },
+  components: {
+    VueC3
   },
   methods: {
     /**
@@ -18,20 +28,60 @@ export default {
      * assign the response data into the state property: dataset
      */
     async GetStockHistories() {
-      this.isRequesting = true;
       try {
-        const result = await this.axios.get("/stocks");
-        this.dataset = result.data;
-      } catch (e) {
-        this.hadError =
-          "There was an issue while fetching the information. Please reload the page.";
+        const stocks = await this.axios.get("/stocks");
+        const volumeColumn = ["Volume"];
+        const lowColumn = ["Low"];
+        const highColumn = ["High"];
+        const adjustedClosePriceColumn = ["Adjusted close price"];
+        const openColumn = ["Open"];
+        const dateColumn = ["Date"];
+        // Format the dataset to C3 structure
+        for (let stock of stocks.data) {
+          volumeColumn.push(stock.volume);
+          lowColumn.push(stock.low);
+          highColumn.push(stock.high);
+          adjustedClosePriceColumn.push(stock.adjustedClosePrice);
+          openColumn.push(stock.open);
+          dateColumn.push(moment(stock.date).format("YYYY-MM-DD"));
+        }
+
+        // console.log(dateColumn);
+        const result = [openColumn, dateColumn];
+        return Promise.resolve(result);
+      } catch (err) {
+        return Promise.reject(err);
       }
-      this.isRequesting = false;
     }
   },
-  created() {
-    // Fecth the initial data
-    this.GetStockHistories();
+  async mounted() {
+    try {
+      // Fecth the initial data
+      const dataset = await this.GetStockHistories();
+      const options = {
+        data: {
+          columns: dataset,
+          x: "Date",
+          y: "High"
+        },
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              format: "%Y-%m-%d"
+            }
+          },
+          y: {
+            tick: {
+              format: format("$,")
+            }
+          }
+        }
+      };
+      this.handler.$emit("init", options);
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 </script>
