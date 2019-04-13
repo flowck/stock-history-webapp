7c69dbf5e5153a9fe1cfd6d566aa385332e504a5
Firmino Changani <flowck96@gmail.com>
2019-04-13 13:30:47 +0100

    Add reset option

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,18 +5,32 @@
       <!-- Limit -->
       <div class="option__block">
         <label>Limit</label>
-        <select v-model="options.limit" @change="ChartOptions(options.limit)">
+        <select
+          v-model="options.limit"
+          @change="GetStocksWithParamQueries(options)"
+        >
           <option :value="50">50</option>
           <option :value="100">100</option>
           <option :value="500">500</option>
         </select>
       </div>
-      <!-- Date range -->
+      <!-- Year -->
       <div class="option__block">
-        <label>Year range</label>
-        <select>
-          <option value=""></option>
+        <label>From</label>
+        <select
+          v-model="options.year"
+          @change="GetStocksWithParamQueries(options)"
+        >
+          <option v-for="year in yearsRange" :key="year" :value="year">{{
+            year
+          }}</option>
         </select>
+      </div>
+      <!-- Reset button -->
+      <div class="option__block">
+        <button :disabled="resetButtonStatus" @click="ResetResults">
+          Reset
+        </button>
       </div>
     </div>
     <vue-c3 :handler="handler" />
@@ -24,6 +38,7 @@
 </template>
 
 <script>
+// Dependencies
 import moment from "moment";
 import Vue from "vue";
 import VueC3 from "vue-c3";
@@ -34,7 +49,8 @@ export default {
     return {
       handler: new Vue(),
       options: {
-        limit: 50
+        limit: 50,
+        year: 2019
       },
       dataset: [],
       isRequesting: false,
@@ -44,23 +60,46 @@ export default {
   components: {
     VueC3
   },
+  computed: {
+    resetButtonStatus() {
+      const { limit, year } = this.options;
+      return limit === 50 && year === 2019 ? true : false;
+    },
+    yearsRange() {
+      const result = [];
+      const currentYear = new Date().getFullYear();
+      const minYear = 1970;
+
+      for (let i = minYear; i <= currentYear; i++) {
+        result.push(i);
+      }
+      return result.reverse();
+    }
+  },
   methods: {
     /**
      * GetStockHistories: This method will fire a GET request and then
      * assign the response data into the state property: dataset
      */
-    async GetStockHistories(limit = 50) {
+    async GetStockHistories(options) {
       try {
-        const query = `limit=${limit}`;
+        // API query options: limit, year
+        const query = options
+          ? `limit=${options.limit}&year=${options.year}`
+          : null;
 
+        // Request the data on /stocks [GET]
         const stocks = await this.axios.get(`/stocks?${query}`);
+
+        // Set the columns
         const volumeColumn = ["Volume"];
         const lowColumn = ["Low"];
         const highColumn = ["High"];
         const adjustedClosePriceColumn = ["Adjusted close price"];
         const openColumn = ["Open"];
         const dateColumn = ["Date"];
-        // Format the dataset to C3 structure
+
+        // Format the dataset to the C3 format
         for (let stock of stocks.data) {
           volumeColumn.push(stock.volume);
           lowColumn.push(stock.low);
@@ -70,16 +109,23 @@ export default {
           dateColumn.push(moment(stock.date).format("YYYY-MM-DD"));
         }
 
-        // console.log(dateColumn);
+        // Return the dataset
         const result = [openColumn, dateColumn];
         return Promise.resolve(result);
       } catch (err) {
         return Promise.reject(err);
       }
     },
-    async ChartOptions(limit) {
+    /**
+     * GetStocksWithParamQueries: This method call the GetStockHistories method
+     * by passing down a parameter object and then dispatch the results to the
+     * chart.
+     *
+     * @param {object} queryOptions
+     */
+    async GetStocksWithParamQueries(queryOptions) {
       try {
-        const result = await this.GetStockHistories(limit);
+        const result = await this.GetStockHistories(queryOptions);
         this.handler.$emit("dispatch", chart => {
           return chart.load({
             columns: result
@@ -89,7 +135,15 @@ export default {
         console.log(err);
       }
     },
-    async InitLineChart() {
+    ResetResults() {
+      this.options = {
+        limit: 50,
+        year: 2019
+      };
+
+      this.GetStocksWithParamQueries(this.options);
+    },
+    async InitChart() {
       try {
         // Fecth the initial data
         const dataset = await this.GetStockHistories();
@@ -120,7 +174,7 @@ export default {
     }
   },
   async mounted() {
-    this.InitLineChart();
+    this.InitChart();
   }
 };
 </script>
