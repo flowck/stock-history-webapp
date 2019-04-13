b775c6baf46e0500c5e262c6a249cfc6af988ab4
Firmino Changani <flowck96@gmail.com>
2019-04-13 10:56:36 +0100

    Bootstrap the application

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,12 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import "../node_modules/c3/c3.css";
+export default {
+  name: "App"
+};
+</script>
+
 <style lang="stylus"></style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import vueAxios from "vue-axios";
 // Custom plugins / components
 import Chart from "v-chart-plugin";
 
 // Configs
+Vue.use(vueAxios, axios);
 Vue.use(Chart);
 Vue.config.productionTip = false;
+
+// Set default configuration of axios
+axios.defaults.baseURL = process.env.VUE_APP_API;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 new Vue({
   router,
