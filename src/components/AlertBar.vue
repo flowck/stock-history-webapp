ac02f6ef720a9b2dd1b0337f600843beed553955
Firmino Changani <flowck96@gmail.com>
2019-04-15 01:32:51 +0100

    Create an alert component

---
+++ src/components/AlertBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="alert" :style="`background-color: ${color};`">
+    <span>{{ message }}</span>
+    <button @click="closeMethod" class="alert__close-btn" title="Close">
+      Close
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlertBar",
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: "#A63232"
+    },
+    closeMethod: {
+      type: Function,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.alert
+  left 0
+  bottom 0
+  width 100%
+  color #FFF
+  display flex
+  position absolute
+  padding 20px 10px
+  justify-content space-between
+
+.alert__close-btn
+  color #FFF
+  cursor pointer
+  padding 5px 10px
+  border-radius 3px
+  border 1px solid #FFF
+  background-color transparent
+</style>
